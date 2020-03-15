bae163fd7e7078b373ca99c70fe708aeedcc9af7
김정민 <dsaqaz421@naver.com>
2020-03-15 12:24:40 +0900

    탄막 게임 커밋
    탄막게임 플레이어 기초 구현

---
+++ Dodge/Assets/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;   // 이동에 사용할 리지드바디 컴포넌트
+    // private으로 바꾸고 나서 인스펙터 창에서 Player Rigidbody 필드가 더이상 표시 안됨. 코드로만 지정한거임.
+    public float speed = 8f;        // 이동 속력
+
+    void Start(){
+        // 게임 오브젝트에서 Rigidbody 컴포넌트를 찾아 playerRigidbody에 할당
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+    
+    // 사용자 입력을 감지하고 playerRigidbody에 힘을 주는 코드
+    void Update(){
+        /*if (Input.GetKey(KeyCode.UpArrow) == true){
+            // 위쪽 방향키 입력이 감지된 경우 z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) == true){
+            // 아래쪽 방향키 입력이 감지된 경우 -z 방향 힘 주기
+            playerRigidbody.AddForce(0f, 0f, -speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) == true){
+            // 오른쪽 방향키 입력이 감지된 경우 x 방향 힘 주기
+            playerRigidbody.AddForce(speed, 0f, 0f);            
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) == true){
+            // 왼쪽 방향키 입력이 감지된 경우 -x 방향 힘 주기
+            playerRigidbody.AddForce(-speed, 0f, 0f);
+        }*/
+
+        // 위의 방식 대신 더 간결하고 조작이 이동속도에 즉시 반영되도록 개선
+        // 수평축과 수직축의 입력값을 감지해서 저장
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        
+        // 실제 이동 속도를 입력값과 이동 속력을 사용해 결정
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        // Vector3 속도를 (xSpeed, 0 , zSpeed)로 생성
+        Vector3 newVelocity = new Vector3(xSpeed, 0f, zSpeed);
+        // 리지드바디의 속도에 newVelocity 할당
+        playerRigidbody.velocity = newVelocity;
+    }
+
+    // 게임 오브젝트를 비활성화하는 die 메서드
+    public void Die(){
+        gameObject.SetActive(false);
+    }
+}
